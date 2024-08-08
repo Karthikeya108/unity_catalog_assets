27c00e5429628f28301270c3aa4f03b4753c8ada
Karthikeya108 <Karthikeya108>
2024-08-08 12:11:32 +0000

    split data generation code and use case code

--- access_controls/fgac/rls/notebooks/UC_RLS_HR_UseCase.sql
+++ access_controls/fgac/rls/notebooks/UC_RLS_HR_UseCase.sql
@@ -5,101 +5,30 @@ USE SCHEMA default;
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC %pip install -q pandas faker mimesis
--- MAGIC dbutils.library.restartPython()
+-- MAGIC #read the files directly from the repo
+-- MAGIC """
+-- MAGIC employees_df = spark.read.option("delimiter", ";").csv("file:/Workspace/Users/[USERNAME]/unity_catalog_assets/access_controls/fgac/rls/data/employees.csv", header=True)
+-- MAGIC managers_df = spark.read.option("delimiter", ";").csv("file:/Workspace/Users/[USERNAME]/unity_catalog_assets/access_controls/fgac/rls/data/managers.csv", header=True)
+-- MAGIC """
+-- MAGIC #OR store the data files in a UC Volume and read it from there
+-- MAGIC employees_df = spark.read.option("delimiter", ";").csv("/Volumes/main/default/data/employees.csv", header=True)
+-- MAGIC managers_df = spark.read.option("delimiter", ";").csv("/Volumes/main/default/data/managers.csv", header=True)
 
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC import pandas as pd
--- MAGIC from typing import Iterator
--- MAGIC from pyspark.sql.functions import *
--- MAGIC from pyspark.sql.types import *
--- MAGIC import time
--- MAGIC from datetime import date
--- MAGIC import random
--- MAGIC from faker import Faker
--- MAGIC from mimesis import Generic
--- MAGIC from mimesis.locales import Locale
+-- MAGIC display(employees_df)
 
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC @pandas_udf("long")
--- MAGIC def get_id(batch_iter: Iterator[pd.Series]) -> Iterator[pd.Series]:
--- MAGIC   for id in batch_iter:
--- MAGIC       yield int(time.time()) + id
-
--- COMMAND ----------
-
--- MAGIC %python
--- MAGIC
--- MAGIC employee_schema = StructType([
--- MAGIC   StructField("employee_id", LongType(), False),
--- MAGIC   StructField("name", StringType(), False),
--- MAGIC   StructField("email", StringType(), False),
--- MAGIC   StructField("gender", StringType(), False),
--- MAGIC   StructField("date_of_birth", DateType(), False),
--- MAGIC   StructField("age", LongType(), False),
--- MAGIC   StructField("address", StringType(), False),
--- MAGIC   StructField("phone_number", StringType(), False),
--- MAGIC   StructField("ssn", StringType(), False),
--- MAGIC   StructField("salary", LongType(), False),
--- MAGIC   StructField("iban", StringType(), False),
--- MAGIC   ])
--- MAGIC
--- MAGIC def generate_fake_employee_data(pdf: pd.DataFrame) -> pd.DataFrame:
--- MAGIC
--- MAGIC   fake = Faker('en_US')
--- MAGIC   generic = Generic(locale=Locale.EN)
--- MAGIC   domain_name = "company.com"
--- MAGIC     
--- MAGIC   def generate_data(y):
--- MAGIC     
--- MAGIC     dob = fake.date_between(start_date='-60y', end_date='-18y')
--- MAGIC
--- MAGIC     y["gender"] = fake.passport_gender()
--- MAGIC     y["name"] = fake.name()
--- MAGIC     y["email"] = fake.email()
--- MAGIC     first_name = fake.first_name_male() if y["gender"] =="M" else fake.first_name_female()
--- MAGIC     last_name = fake.last_name()
--- MAGIC     y["name"] = f"{first_name} {last_name}"
--- MAGIC     y["email"] = f"{first_name}.{last_name}@{domain_name}"
--- MAGIC     y["date_of_birth"] = dob #.strftime("%Y-%m-%d")
--- MAGIC     y["age"] = date.today().year - dob.year
--- MAGIC     y["address"] = fake.address()
--- MAGIC     y["phone_number"] = fake.phone_number()
--- MAGIC     y["ssn"] = fake.ssn()
--- MAGIC     y["salary"] = fake.random_int(min=65000, max=900000)
--- MAGIC     y["iban"] = fake.iban()
--- MAGIC
--- MAGIC     return y
--- MAGIC     
--- MAGIC   return pdf.apply(generate_data, axis=1).drop(["partition_id", "id"], axis=1)
-
--- COMMAND ----------
-
--- MAGIC %python
--- MAGIC num_rows = 10
-
--- COMMAND ----------
-
--- MAGIC %python
--- MAGIC initial_data = spark.range(1, num_rows+1).withColumn("employee_id", get_id(col("id")))
--- MAGIC employees_df = (initial_data
--- MAGIC   .withColumn("partition_id", spark_partition_id())
--- MAGIC   .groupBy("partition_id")
--- MAGIC   .applyInPandas(generate_fake_employee_data, employee_schema)
--- MAGIC   .orderBy(asc("employee_id")))
+-- MAGIC display(managers_df)
 
 -- COMMAND ----------
 
 -- MAGIC %python
 -- MAGIC employees_df.write.mode("overwrite").saveAsTable("employees")
-
--- COMMAND ----------
-
-SELECT * FROM employees;
+-- MAGIC managers_df.write.mode("overwrite").saveAsTable("managers")
 
 -- COMMAND ----------
 
@@ -107,23 +36,6 @@ SELECT * FROM employees;
 -- MAGIC ###Employee Hierarchy
 -- MAGIC
 -- MAGIC <img src="https://github.com/Karthikeya108/unity_catalog_assets/raw/main/access_controls/fgac/rls/images/hierarchy.png" width="800">
-
--- COMMAND ----------
-
-CREATE OR REPLACE TABLE managers
-  (employee_id LONG, employee_email STRING, direct_report LONG);
-
-INSERT INTO managers
-VALUES
-  (1723035680, 'Jessica.Carey@company.com', 1723035681),
-  (1723035680, 'Jessica.Carey@company.com', 1723035679),
-  (1723035680, 'Jessica.Carey@company.com', 1723035686),
-  (1723035681, 'Kayla.Johnson@company.com', 1723035684),
-  (1723035681, 'Kayla.Johnson@company.com', 1723035677),
-  (1723035679, 'Jessica.Robinson@company.com', 1723035685),
-  (1723035686, 'Martha.Weiss@company.com', 1723035682),
-  (1723035685, 'Lisa.Sanders@company.com', 1723035683),
-  (1723035683, 'Danielle.Gibbs@company.com', 1723035678);
 
 -- COMMAND ----------
 
@@ -158,16 +70,11 @@ DROP FUNCTION IF EXISTS row_filter_direct_reports;
 --If the row filter refers to one or more tables, you cannot apply the row filter to any of those tables.
 CREATE FUNCTION row_filter_direct_reports(manager STRING)
 RETURNS BOOLEAN
-  --RETURN (manager = CURRENT_USER());
-  RETURN 1
+  RETURN (manager = CURRENT_USER());
 
 -- COMMAND ----------
 
 ALTER TABLE employees SET ROW FILTER row_filter_direct_reports ON (manager);
-
--- COMMAND ----------
-
-SELECT CURRENT_USER();
 
 -- COMMAND ----------
 
@@ -210,8 +117,7 @@ SELECT * FROM employee_manager_mulitple_levels ORDER BY manager_level;
 -- COMMAND ----------
 
 CREATE OR REPLACE FUNCTION row_filter_reports(email STRING)
-  --RETURN (email = CURRENT_USER()) 
-  RETURN TRUE
+  RETURN (email = CURRENT_USER())
   OR (email IN (SELECT emml.employee_email
  FROM main.default.employee_manager_mulitple_levels emml
     WHERE emml.manager_level = CURRENT_USER()));
@@ -223,10 +129,6 @@ ALTER TABLE employees SET ROW FILTER row_filter_reports ON (email);
 -- COMMAND ----------
 
 SELECT * FROM employees ORDER BY manager;
-
--- COMMAND ----------
-
-
 
 -- COMMAND ----------
 
---
+++ common/notebooks/Generate_Data.py
@@ -0,0 +1,99 @@
+# Databricks notebook source
+# MAGIC %pip install -q pandas faker mimesis
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+import pandas as pd
+from typing import Iterator
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import time
+from datetime import date
+import random
+from faker import Faker
+from mimesis import Generic
+from mimesis.locales import Locale
+
+# COMMAND ----------
+
+@pandas_udf("long")
+def get_id(batch_iter: Iterator[pd.Series]) -> Iterator[pd.Series]:
+  for id in batch_iter:
+      yield int(time.time()) + id
+
+# COMMAND ----------
+
+employee_schema = StructType([
+  StructField("employee_id", LongType(), False),
+  StructField("name", StringType(), False),
+  StructField("email", StringType(), False),
+  StructField("gender", StringType(), False),
+  StructField("date_of_birth", DateType(), False),
+  StructField("age", LongType(), False),
+  StructField("address", StringType(), False),
+  StructField("phone_number", StringType(), False),
+  StructField("ssn", StringType(), False),
+  StructField("salary", LongType(), False),
+  StructField("iban", StringType(), False),
+  ])
+
+def generate_fake_employee_data(pdf: pd.DataFrame) -> pd.DataFrame:
+
+  fake = Faker('en_US')
+  generic = Generic(locale=Locale.EN)
+  domain_name = "company.com"
+    
+  def generate_data(y):
+    
+    dob = fake.date_between(start_date='-60y', end_date='-18y')
+
+    y["gender"] = fake.passport_gender()
+    y["name"] = fake.name()
+    y["email"] = fake.email()
+    first_name = fake.first_name_male() if y["gender"] =="M" else fake.first_name_female()
+    last_name = fake.last_name()
+    y["name"] = f"{first_name} {last_name}"
+    y["email"] = f"{first_name}.{last_name}@{domain_name}"
+    y["date_of_birth"] = dob #.strftime("%Y-%m-%d")
+    y["age"] = date.today().year - dob.year
+    y["address"] = fake.address()
+    y["phone_number"] = fake.phone_number()
+    y["ssn"] = fake.ssn()
+    y["salary"] = fake.random_int(min=65000, max=900000)
+    y["iban"] = fake.iban()
+
+    return y
+    
+  return pdf.apply(generate_data, axis=1).drop(["partition_id", "id"], axis=1)
+
+# COMMAND ----------
+
+num_rows = 1000
+
+# COMMAND ----------
+
+#generate the dataset
+initial_data = spark.range(1, num_rows+1).withColumn("employee_id", get_id(col("id")))
+employees_df = (initial_data
+  .withColumn("partition_id", spark_partition_id())
+  .groupBy("partition_id")
+  .applyInPandas(generate_fake_employee_data, employee_schema)
+  .orderBy(asc("employee_id")))
+
+# COMMAND ----------
+
+#persist the data
+employees_df.write.mode("overwrite").saveAsTable("main.default.employees")
+
+# COMMAND ----------
+
+#export data to CSV files
+employees_df = employees_df.toPandas().replace(r'\n',  ' ', regex=True)
+employees_df.to_csv("/Volumes/main/default/data/employees.csv", sep=";", index=False)
+
+# COMMAND ----------
+
+# MAGIC %environment
+# MAGIC "client": "1"
+# MAGIC "base_environment": ""
